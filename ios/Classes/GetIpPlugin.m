e0b8b1d108bd41553799ec9938b143529eb0bd13
Joo Aun Saw <jasaw@dius.com.au>
2020-09-14 16:55:01 +1000

    fix include failure

--- ios/Classes/GetIpPlugin.m
+++ ios/Classes/GetIpPlugin.m
@@ -1,5 +1,9 @@
 #import "GetIpPlugin.h"
+#if __has_include(<get_ip/get_ip-Swift.h>)
+#import <get_ip/get_ip-Swift.h>
+#else
 #import "get_ip-Swift.h"
+#endif
 
 @implementation GetIpPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
